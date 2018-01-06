540d07227b788eef70d97b2c90bc490dd84a797b
ovimughal <ovi.mughal@gmail.com>
2018-01-06 10:37:30 +0530

    login enable & disable feature

--- dist/oapiconfig.global.php
+++ dist/oapiconfig.global.php
@@ -3,15 +3,16 @@
 return [
     'oconfig_manager' => [
         'settings' => [
+            'enable_login' => false,
             'image_server' => 'http://localhost:port/',
             'customeName1_image_path' => 'img/customeName1/',
             'customeName2_image_path' => 'img/customeName2/',
         ],
         'api' => [
-            'api_key' => '<api-key>',
+            'api_key' => '<api-key>',//base64_encode(openssl_random_pseudo_bytes(64))
         ],
         'ojwt' => [
-            'jwt_key' => '<token>', //base64_encode(openssl_random_pseudo_bytes(64]],
+            'jwt_key' => '<token>', //base64_encode(openssl_random_pseudo_bytes(64))
             'algo' => 'HS512',
             'server' => 'http://localhost:port/',
             'iatOffset' => 10,
